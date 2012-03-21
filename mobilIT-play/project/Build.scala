2747b35555c1f164e6a0184d3dcc6379ec5dbb8e
andy petrella <andy.petrella@gmail.com>
2012-03-21 21:10:15 +0100

    Intégration of Bootstrap2.0 in Play module. Update the main template to integrate this change with a very simple usage.

--- mobilIT-play/project/Build.scala
+++ mobilIT-play/project/Build.scala
@@ -7,12 +7,18 @@ object ApplicationBuild extends Build {
     val appName         = "mobilIT"
     val appVersion      = "1.0-SNAPSHOT"
 
+    // Only compile the bootstrap bootstrap.less file and any other *.less file in the stylesheets directory
+    def customLessEntryPoints(base: File): PathFinder = (
+      (base / "app" / "assets" / "stylesheets" / "bootstrap" * "bootstrap.less") +++
+      (base / "app" / "assets" / "stylesheets" * "*.less")
+    )
+
     val appDependencies = Seq(
       // Add your project dependencies here,
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+      lessEntryPoints <<= baseDirectory(customLessEntryPoints)      
     )
 
 }
