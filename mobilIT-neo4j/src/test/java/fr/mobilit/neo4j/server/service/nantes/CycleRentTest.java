e3c72a81f1b8df716f8b80e0cfd32e83d687f160
Noootsab <andy.petrella@nextlab.be>
2012-06-02 13:57:03 +0200

    change a bit the test checks to match the changing xml file contents from bicloo

--- mobilIT-neo4j/src/test/java/fr/mobilit/neo4j/server/service/nantes/CycleRentTest.java
+++ mobilIT-neo4j/src/test/java/fr/mobilit/neo4j/server/service/nantes/CycleRentTest.java
@@ -13,14 +13,17 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with MobilIT. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * @See https://github.com/sim51/mobilIT
  */
 package fr.mobilit.neo4j.server.service.nantes;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -33,22 +36,63 @@ import fr.mobilit.neo4j.server.utils.Constant;
 
 public class CycleRentTest extends Neo4jTestCase {
 
+    private int numberOfStations = 0;
+
+
     @BeforeClass
     public void setUp() throws Exception {
         super.setUp(true);
+        HttpClient client = new HttpClient();
+        GetMethod get = null;
+        try {
+            // we do the http call and parse the xml response
+            get = new GetMethod(CycleRentImpl.IMPORT_URL);
+            client.executeMethod(get);
+            javax.xml.stream.XMLInputFactory factory = javax.xml.stream.XMLInputFactory.newInstance();
+            javax.xml.stream.XMLStreamReader parser = factory.createXMLStreamReader(get.getResponseBodyAsStream());
+            ArrayList<String> currentXMLTags = new ArrayList<String>();
+            int depth = 0;
+            while (true) {
+                int event = parser.next();
+                if (event == javax.xml.stream.XMLStreamConstants.END_DOCUMENT) {
+                    break;
+                }
+                switch (event) {
+                    case javax.xml.stream.XMLStreamConstants.START_ELEMENT:
+                        currentXMLTags.add(depth, parser.getLocalName());
+                        String tagPath = currentXMLTags.toString();
+                        // here we have a match, so we construct the POI
+                        if (tagPath.equals("[carto, markers, marker]")) {
+                            numberOfStations++;
+                        }
+                        depth++;
+                        break;
+                    case javax.xml.stream.XMLStreamConstants.END_ELEMENT:
+                        depth--;
+                        currentXMLTags.remove(depth);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e.getCause());
+        } finally {
+            get.releaseConnection();
+        }
     }
 
     @Test
     public void testImport() throws MobilITException {
         CycleRentImpl nantes = new CycleRentImpl(this.spatial());
         List<POI> bicloo = nantes.importStation();
-        assertEquals(103, bicloo.size());
+        assertEquals(numberOfStations, bicloo.size());
     }
 
     @Test
     public void testStation() throws MobilITException {
         CycleRentImpl nantes = (CycleRentImpl) CycleRent.getService(this.spatial(), Constant.NANTES_GEO_CODE);
-        Map<String, Integer> result = nantes.getStation("103");
+        Map<String, Integer> result = nantes.getStation(""+numberOfStations);
         assertNotNull(result.get(Constant.CYCLE_AVAIBLE));
         assertNotNull(result.get(Constant.CYCLE_FREE));
         assertNotNull(result.get(Constant.CYCLE_TOTAL));
